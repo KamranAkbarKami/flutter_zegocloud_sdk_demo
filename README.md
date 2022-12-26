a11f808b5867a18359386d8de9605530a47197b6
KamranAkbarKami <76488599+KamranAkbarKami@users.noreply.github.com>
2022-12-26 13:06:06 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# flutter_zegocloud_demo
+
+Flutter Project to Help integrate Zego Cloud Sdk for Video and Voice Call Meetings.
+
+
---
+++ android/app/src/main/kotlin/com/kamran/flutter_zegocloud_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kamran.flutter_zegocloud_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
